667db4d4c399a270335796e2e85a281d5822b151
diskrot <144980221+diskrot@users.noreply.github.com>
2024-11-01 22:54:21 -0300

    Fixed stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,8 @@ public class PlayerController : MonoBehaviour
     public InputActionReference movementInput;
     public Vector2 movement2d;
     private Rigidbody2D rigidbody2d;
-    public Animator animator;
+    public Animator playerAnim;
+    public Animator weaponAnim;
     public float speed = 1f;
 
     // Start is called before the first frame update
@@ -24,11 +25,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movement2d = movementInput.action.ReadValue<Vector2>();
-        animator.SetFloat("xspeed",movement2d.x);
-        animator.SetFloat("yspeed",movement2d.y);
+        playerAnim.SetFloat("xspeed",movement2d.x);
+        playerAnim.SetFloat("yspeed",movement2d.y);
     }
     void FixedUpdate()
     {
         rigidbody2d.AddForce(movement2d * speed);
+        if (Input.GetMouseButton(0))
+        {
+            weaponAnim.SetFloat("Blend", 1f); // Agrandar mientras se mantiene presionado
+        }
+        else
+        {
+            weaponAnim.SetFloat("Blend", 0f); // Volver al tamaño original cuando se suelta
+        }
     }
 }
